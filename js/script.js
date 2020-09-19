22cfa223ec24fe4bf178d5af13bd333930a35342
nguyenhaidone <dang682000@gmail.com>
2020-09-19 08:21:36 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,111 @@
-$('.carousel').carousel({
-  interval: 5000
-})
+
+function showFeeS3Door(){
+  document.getElementById("showGia").innerText="	1,769,000,000đ";
+  document.getElementById("showPtb").innerText="	176,900,000đ";
+  document.getElementById("showPsd").innerText="	1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="	1,959,330,000đ";
+}
+
+
+function showFeeS5Door(){
+  document.getElementById("showGia").innerText="1,819,000,000đ";
+  document.getElementById("showPtb").innerText="181,900,000đ";
+  document.getElementById("showPsd").innerText="	1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="2,014,330,000đ";
+}
+
+function showFeeSConvertible(){
+  document.getElementById("showGia").innerText="2,189,000,000đ";
+  document.getElementById("showPtb").innerText="218,900,000đ";
+  document.getElementById("showPsd").innerText="	1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="2,421,330,000đ";
+}
+
+function showFeeSClubman(){
+  document.getElementById("showGia").innerText="2,249,000,000đ";
+  document.getElementById("showPtb").innerText="224,900,000đ";
+  document.getElementById("showPsd").innerText="	1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="2,487,330,000đ";
+}
+
+function showFeeSCountryman(){
+  document.getElementById("showGia").innerText="1,589,000,000đ";
+  document.getElementById("showPtb").innerText="158,900,000đ";
+  document.getElementById("showPsd").innerText="	1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="1,761,330,000đ";
+}
+
+function showFeeOLci(){
+  document.getElementById("showGia").innerText="1,529,000,000đ";
+  document.getElementById("showPtb").innerText="152,900,000đ";
+  document.getElementById("showPsd").innerText="1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="1,695,330,000đ";
+}
+
+function showFeeLci3(){
+  document.getElementById("showGia").innerText="1,969,000,000đ";
+  document.getElementById("showPtb").innerText="196,900,000đ";
+  document.getElementById("showPsd").innerText="1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="2,179,330,000đ";
+}
+
+function showFeeLci5(){
+  document.getElementById("showGia").innerText="2,049,000,000đ";
+  document.getElementById("showPtb").innerText="204,900,000đ";
+  document.getElementById("showPsd").innerText="1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="2,267,330,000đ";
+}
+
+function showFeeJcw3St(){
+  document.getElementById("showGia").innerText="	2,329,000,000đ";
+  document.getElementById("showPtb").innerText="232,900,000đ";
+  document.getElementById("showPsd").innerText="1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="2,575,330,000đ";
+}
+
+function showFeeCountrySt(){
+  document.getElementById("showGia").innerText="2,699,000,000đ";
+  document.getElementById("showPtb").innerText="269,900,000đ";
+  document.getElementById("showPsd").innerText="1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="2,982,330,000đ";
+}
+
+function showFeeCountryH(){
+  document.getElementById("showGia").innerText="2,719,000,000đ";
+  document.getElementById("showPtb").innerText="271,900,000đ";
+  document.getElementById("showPsd").innerText="1,560,000đ";
+  document.getElementById("showBh").innerText="	530,000đ";
+  document.getElementById("showPdangky").innerText="	11,000,000đ";
+  document.getElementById("showPdangkiem").innerText="	340,000đ";
+  document.getElementById("showTotal").innerText="3,004,330,000đ";
+}
